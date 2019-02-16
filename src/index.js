9719c4c17e8e429dfc3de706c2a548f333bbd082
MarquesDev <marques.developpement@gmail.com>
2019-02-16 14:12:21 +0100

    chore: publish tests utils

---
+++ src/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  FindDefaultFeeds: require("./models/feed/FindDefaultFeeds"),
+  FindFeedsOfUser: require("./models/feed/FindFeedsOfUser"),
+  InsertFeed: require("./models/feed/InsertFeed"),
+  FindUserByEmailAndPassword: require("./models/user/FindUserByEmailAndPassword"),
+  InsertUserToFeed: require("./models/user-to-feed/InsertUserToFeed")
+};
---
+++ utils/index.js
@@ -0,0 +1,4 @@
+module.exports.buildResponseFromSchema = require("../src/utils/for-tests/build-response-from-schema");
+module.exports.mockDbResponse = require("../src/utils/for-tests/mock-db-response");
+module.exports.mockFailResponse = require("../src/utils/for-tests/mock-fail-response");
+module.exports.testModelSchema = require("../src/utils/for-tests/test-model-schema");
